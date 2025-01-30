6ac01af17ac843873dc24cdddf8b3624c3547178
jennifer <jennifersantossouza@hotmail.com>
2025-01-30 18:21:28 -0300

    feat: implemanting the generate password function and some popups to confirmation

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
-# PASSWORD GENERATOR
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = (
+        [choice(letters) for _ in range(randint(8, 10))] +
+        [choice(symbols) for _ in range(randint(2, 4))] +
+        [choice(numbers) for _ in range(randint(2, 4))]
+    )
+
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 
 def save():
@@ -9,15 +27,15 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    if len(website) <= 0 and len(password) <= 0:
-        messagebox.showinfo(title="Empty fields", message="Please don't leave any field empty!")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Ops", message="Please don't leave any field empty!")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n"f" Email: {email}\n"
                                                               f" Password: {password} \n Is it ok to save?")
         if is_ok:
             with open("data.txt", "a") as file:
                 file.write(f"{website} | {email} | {password} \n\n")
-                # clear the inputs fields
+                # clear the input fields
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
@@ -29,9 +47,8 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=0, columnspan=3)  # Centraliza a logo sobre as 3 colunas
+canvas.grid(row=0, column=0, columnspan=3)
 
-# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0, sticky="e")
 email_label = Label(text="Email/Username:")
@@ -39,7 +56,6 @@ email_label.grid(row=2, column=0, sticky="e")
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0, sticky="e")
 
-# Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
 website_entry.focus()
@@ -49,8 +65,7 @@ email_entry.insert(0, "jennifer@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="ew")
 
-# Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="ew")
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
