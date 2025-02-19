3a81c4f55e115f8edea6b16661691a34b6b0ee8a
jennifer <jennifersantossouza@hotmail.com>
2025-02-19 16:15:18 -0300

    feat: implementing the search button

--- main.py
+++ main.py
@@ -47,10 +47,24 @@ def save():
             data.update(new_data)
             with open("data.json", "w") as file:
                 json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
-
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f'No details for "{website}" exists.')
 
 window = Tk()
 window.title("Password Manager")
@@ -68,8 +82,8 @@ email_label.grid(row=2, column=0, sticky="e")
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0, sticky="e")
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, sticky="ew")
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
@@ -81,6 +95,8 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2, sticky="ew")
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1 ,column=2, sticky="ew")
 
 window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
